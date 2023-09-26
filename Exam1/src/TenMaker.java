8e0b813c7e1fbe9beb7f0add7271bb7ab6ec16ff
andeki <143848894+andeki@users.noreply.github.com>
2023-09-26 14:42:31 -0600

    Update TenMaker.java

--- Exam1/src/TenMaker.java
+++ Exam1/src/TenMaker.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
 
 public class TenMaker {
-    public static boolean TenMaker(int a, int b)
-    {
-        if(a + b == 10)
-        {
+    public static boolean TenMaker(int a, int b) {
+        if(a + b == 10) {
             return true;
         }
         return false;
